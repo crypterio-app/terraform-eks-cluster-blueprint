db9359a56a3aff6485a5ddcc6edf3088f009b278
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-26 00:25:03 +0300

    update

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
 
+terraform {
+  backend "s3" {
+    bucket = "eks-cluster-dev01" # Specify the name of the S3 bucket to store the state file
+    region = "us-west-2"
+    key = "eks/terraform.tfstate"
+  }
+}
+
