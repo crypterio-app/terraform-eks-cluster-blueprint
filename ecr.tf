a8b0df64e8314996db6603418ffa80d11829c96b
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-26 00:24:09 +0300

    update

--- ecr.tf
+++ ecr.tf
@@ -1,1 +1,9 @@
 
+# Create an ECR repository
+resource "aws_ecr_repository" "nginx_repo" {
+  name                 = var.ecr_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
