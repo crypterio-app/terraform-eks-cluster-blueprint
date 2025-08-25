6700a4de9e9530c6565185c09a9341c7df8a124c
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-26 00:23:42 +0300

    update

--- eks.tf
+++ eks.tf
@@ -1,1 +1,56 @@
 
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 20.0"
+    cluster_name = var.cluster_name
+    cluster_version = var.cluster_version
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.my-vpc.vpc_id
+    subnet_ids = module.my-vpc.private_subnets
+
+    access_entries = {
+        jenkins_user = {
+            kubernetes_groups = []
+            principal_arn     = var.creator_principal_arn
+
+            policy_associations = {
+                example = {
+                    policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+                    access_scope = {
+                        type       = "cluster"
+                    }
+                }   
+            }
+        },
+        aws_console_user = {
+            kubernetes_groups = []
+            principal_arn     = var.console_user_principal_arn
+            policy_associations = {
+                example = {
+                    policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+                    access_scope = {
+                        type       = "cluster"
+                    }
+                }   
+            }
+        }
+    }
+
+
+    tags = {
+        environment = "development"
+        application = "nginx-app"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 1
+            desired_size = 1
+
+            instance_types = ["t2.small"]
+        }
+    }
+}
