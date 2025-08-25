51c9ee938b63837ac9ef3b8c4c2a6cfb40c484a3
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-26 01:35:04 +0300

    update

--- eks.tf
+++ eks.tf
@@ -10,17 +10,6 @@ module "eks" {
     vpc_id = module.my-vpc.vpc_id
     subnet_ids = module.my-vpc.private_subnets
 
-    access_entries = {
-    terraform_runner = {
-      principal_arn = data.aws_caller_identity.current.arn
-      policy_associations = {
-        cluster_admin = {
-          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = { type = "cluster" }
-        }
-      }
-    }
-
     tags = {
         environment = "development"
         application = "nginx-app"
