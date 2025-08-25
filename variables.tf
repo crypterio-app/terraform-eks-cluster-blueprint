3630a1aeba5ef642e94a696e7b511c2aa3043dfb
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-26 00:20:30 +0300

    update

--- variables.tf
+++ variables.tf
@@ -1,1 +1,37 @@
 
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "List of CIDR blocks for the private subnets"
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "List of CIDR blocks for the public subnets"
+}
+
+variable "ecr_name" {
+  description = "Name of the ECR repository"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+}
+
+variable "cluster_version" {
+  description = "Version of the EKS cluster"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+variable "creator_principal_arn" {
+  description = "IAM user ARN who created the EKS cluster"
+  type        = string
+}
+
+variable "console_user_principal_arn" {
+  description = "IAM user/role ARN who has console access to the EKS cluster"
+  type        = string
+}
